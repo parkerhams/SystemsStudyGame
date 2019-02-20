ae9347b63ab91d85428d8503f9a35dae595f359e
Parker Hamilton <PARKER.HAMILTON@iam.local>
2019-02-20 06:13:30 -0600

    Edited Player Movement and Camera Controller  - having issues right now with playability, but by freezing z positon on player, we are finally able to move in camera direction whether it's up or down or left to right. Added Post processing via package manager. In our scripts, we've used AddForce() on player rigidbody to call our input and "gas pedal" value of thrust. In CameraController, we set the camera position to follow the player while the player movement reads the camera information for alignment sake and for forward motion in cam direction

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //player transform values set in inspector
+    public Transform target;
+    //distance camera is away from player
+    public Vector3 offset;
+    //to be used later if we need to halt movement in any way
+    public bool useOffsetValues;
+    //how quickly camera rotates around target
+    public float rotateSpeed;
+    //to set a minimum value the camera cannot go below
+    public float minZHeight = .5f;
+    //pivot for rotation and alignment - an empty
+    public Transform pivot;
+
+    void Start()
+    {
+        if(!useOffsetValues)
+        {
+            //splitting the difference between player position and camera position
+            offset = target.position - transform.position;
+        }
+
+        //set this here in case we want to change where the pivot goes later
+        pivot.transform.position = target.transform.position;
+        //childed pivot can be send to the target at runtime
+        pivot.transform.parent = target.transform;
+
+        //get rid of mouse icon during play
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    //with help from tutorial by gamesplusjames, learned LateUpdate is called after each frame of Update
+    //useful for realigning the camera after input instead of during to avoid stutter
+    void LateUpdate()
+    {
+        //get x position of mouse and rotate target
+        float horizontalInput = Input.GetAxis("Mouse X") * rotateSpeed;
+        //rotate the target based on the horizontal input of the mouse
+        target.Rotate(0, horizontalInput, 0);
+
+        //get y position of mouse and rotate the pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        //take rotation from pivot and apply to camera itself
+        pivot.Rotate(-vertical, 0, 0);
+
+        //move camera based on position of current target rotation and original offset
+        float desiredYAngle = target.eulerAngles.y;
+        float desiredXAngle = target.eulerAngles.x;
+        //create a rotation quaternion that will take our desired angle vlaues to be smoother
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        //move our camera to our taget smoothly while keeping mind its offset and current rotation
+        transform.position = target.position - (rotation * offset);
+
+        //send cam to the target if past a certain distance
+        if(transform.position.y < target.position.y)
+        {
+            //make sure it's within the bounds we set
+            transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z - minZHeight);
+        }
+
+        //focus on player - could be used for other things later, too, like plants or trees to grow 
+        transform.LookAt(target);
+
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //basically accelerate value, the gas pedal
+    public float thrust;
+    //how fast cam moves
+    public float camSpeed = 180;
+    public Rigidbody rb;
+
+    //actually calling vector3 position values of the main camera
+    public Transform cam;
+
+    float heading = 0;
+    //movement!
+    Vector3 inputVector;
+    //cam location / position values
+    Vector3 camForward;
+    Vector3 camRight;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        //right nw, isnce CameraRotation() is not called and causes so many issues, these are redundant - but here for revision
+        camForward = cam.forward;
+        camRight = cam.right;
+
+    }
+
+    void FixedUpdate()
+    {
+        //set the input to match the forward value as we call each input axis
+        inputVector = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        //normalize it so the value does not stutter, multiply by the thrust to get speed changes
+        inputVector = inputVector.normalized * thrust;
+
+        //CamRotation();
+
+        //using AddForce, we multiply the input (vertical and horziontal) by the thrust
+        rb.AddForce(inputVector * thrust);
+    }
+
+    //followed along with tutorial from lmhpoly - but now calling this function causes bad camera glitches. 
+    //Keeping for reference later
+    void CamRotation()
+    {
+        //heading, in this case, is meant to take in the mouse X and set a movement speed for the camera
+        heading += Input.GetAxis("Mouse X") * Time.deltaTime*camSpeed;
+        //with a euler angle, calling that rotation to be set
+        cam.rotation = Quaternion.Euler(0, heading, 0);
+
+        //inputVector is basically referencing the direct input of player and setting it a new vector 3 that calls the input axis
+        inputVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        //clamp magnitude being called so we can bind it to this value
+        inputVector = Vector3.ClampMagnitude(inputVector, 1);
+
+        //calling these values and normalizing them sets a limit on y movement to avoid unwated rotation, ideally
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camRight = camRight.normalized;
+        camForward = camForward.normalized;
+
+        //calling the forward transform of the player to be added based on the cam axes and input axes
+        transform.forward += (transform.position += (camForward * inputVector.y + camRight * inputVector.x) * Time.deltaTime * thrust);
+
+        //using Vector3s to more directly set the cam/player positions together
+        Vector3 relativePos = cam.position - transform.position;
+        Quaternion playerCamRotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        transform.rotation = playerCamRotation;
+
+    }
+}
