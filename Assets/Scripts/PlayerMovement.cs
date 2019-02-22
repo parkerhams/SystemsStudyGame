7fb75ff7975b79a9f4707e4a0821905b5bb26a1e
Parker Hamilton <PARKER.HAMILTON@iam.local>
2019-02-22 13:49:24 -0600

    spaces between serialized variables - consistency's sake

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,13 +7,15 @@ public class PlayerMovement : MonoBehaviour
     [Tooltip("float values for drag - moving drag")]
     [SerializeField]
     private float movingDrag;
+
     [Tooltip("float values for drag - stopping drag")]
     [SerializeField]
     private float stoppingDrag;
-  
+    
     [Tooltip("basically accelerate value, the gas pedal")]
     [SerializeField]
     float thrust;
+
     [SerializeField]
     Rigidbody playerRigidbody;
 
