471050cdaffdeed4847df35d0ae4087028bb0786
Parker Hamilton <PARKER.HAMILTON@iam.local>
2019-02-22 13:47:01 -0600

    Code review response - got rid of commented out code, white space, pointless organization

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,33 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    //float values for drag - moving drag and stopping drag
+    [Tooltip("float values for drag - moving drag")]
     [SerializeField]
     private float movingDrag;
+    [Tooltip("float values for drag - stopping drag")]
     [SerializeField]
     private float stoppingDrag;
-
-
-    ///<summary> Meant to affect player movement, we are using the 
-    ///thrust as a gas pedal, the rigidbody for our physics and force,
-    ///and the camSpeed to affect our thrust further.
-    #region Serialized Variables to control player movement
+  
     //basically accelerate value, the gas pedal
     [SerializeField]
     float thrust;
     [SerializeField]
     Rigidbody playerRigidbody;
-    #endregion
-    ///</summary>
-
-    #region Reading Camera values to determine direction
-    //actually calling vector3 position values of the main camera
-    //heading is calling
-    //[SerializeField]
-    //float heading = 0;
-    //movement!
-    //cam location / position values with cam.forward and cam.right
-    #endregion
 
     void Start()
     {
@@ -39,21 +24,15 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-
         //using AddForce, we multiply the input (vertical and horziontal) by the thrust
-        //playerRigidbody.AddForce(inputVector * thrust);
         if (Input.GetButton("Thrust"))
         {
-            playerRigidbody.drag = movingDrag;
-            
+            playerRigidbody.drag = movingDrag;            
             playerRigidbody.AddForce(playerRigidbody.transform.forward * thrust);
         }
         else
         {
             playerRigidbody.drag = stoppingDrag;
         }
-
-        //Basically, the simplest addforce of camera.forward is dependent on a camera follow script -
-        //for some reason, not really working on play if that follow script is deactivated in the editor
     }
 }
