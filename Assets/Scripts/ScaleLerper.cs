3abdb58b9f0f5510234ad508d596bc2378e51f60
parkerhams <parker.hamilton@loop.colum.edu>
2019-04-08 21:13:17 -0500

    made changes to variable names and added summaries in our ScaleLerper class to make more sense of things

--- Assets/Scripts/ScaleLerper.cs
+++ Assets/Scripts/ScaleLerper.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The scale leraper class is meant to scale a game object, in this case trees, upwards to a maxScale
+/// while the player's game object is in the trigger area of the tree.
+/// The trees are referred to as a GameObject scalableObject, which grows to the maxScale variable.
+/// The trees will grow ar a rate of the speed variable over a duration of the duration variable. 
+/// Audio will play while the game object scales upwards, and it will play particle effects to show it's reached maxScale.
+/// </summary>
 public class ScaleLerper : MonoBehaviour
 {
     [Tooltip("base size for any trees or foliage that will scale")]
@@ -30,7 +37,7 @@ public class ScaleLerper : MonoBehaviour
     AudioSource completedGrowingAudio;
 
     [SerializeField]
-    ParticleSystem doneGrowingParticles;
+    ParticleSystem growthCompletedParticles;
 
     [Tooltip("How long the doneGrowingParticles should play for")]
     private float particleDuration = 2f;
@@ -64,7 +71,13 @@ public class ScaleLerper : MonoBehaviour
     //takes in two vector 3s - start scale and max scale - and how quickly they lerp as time
     public IEnumerator RepeatLerp(Vector3 a, Vector3 b, float time)
     {
-        //rate of growth
+        /// <summary>
+        /// This Enumerator is using scaleLerp to tell the gameObject where to start - and when we reach
+        /// the iterateLerp variable, in this case 1 divided by the passed in time variable multipled by speed,
+        /// we are telling the game objects to stop scaling. While we scale upwards, we scale while we are 
+        /// between the values of scaleRate and iterateLerpRate. We start at a value of 0 here, and once the value reaches 
+        /// the iterateLerpRate value, we stop scaling upwards.
+        ///</summary>
         float scaleRate = 0.0f;
         float iterateLerpRate = (1.0f / time) * speed;
 
@@ -74,16 +87,16 @@ public class ScaleLerper : MonoBehaviour
             scaleRate += Time.deltaTime * iterateLerpRate;
 
             //changing the world scale of the object to whatever it is on those three conditions
-            scalableObject.transform.localScale = Vector3.Lerp(a, b, i);
+            scalableObject.transform.localScale = Vector3.Lerp(a, b, scaleRate);
 
             //set it to not repeatable once the cycle is done         
             yield return null;
         }
 
-        doneGrowingParticles.Play();
+        growthCompletedParticles.Play();
         yield return new WaitForSeconds(particleDuration);
         completedGrowingAudio.Play();
-        doneGrowingParticles.Stop();
+        growthCompletedParticles.Stop();
         auraAudio.Stop();
     }
 
